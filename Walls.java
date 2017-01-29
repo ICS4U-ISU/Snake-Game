418302e4045935a7853fa2e305c41b8db247397c
Pranav Gogia <gogiapranav@gmail.com>
2017-01-29 18:28:20 -0500

    Just added the group members names

--- Walls.java
+++ Walls.java
@@ -1,3 +1,8 @@
+// Team SNAP
+// Pranav G, Maryam S, Ajay G and Jennifer N.
+// Snake Game
+// This program is a reimagining of the classic game: "Snake"
+// 25 January 2017
 import java.io.FileInputStream;
 
 import sun.audio.AudioStream;
@@ -11,11 +16,13 @@ public class Walls extends NonMoving {
 	}
 
 	public void hitWall() {
+		// initializing the name of the sound for when the snakes hits the wall or the obstacle
 		try {
 			AudioStream hitSound;
 			hitSound = new AudioStream(new FileInputStream("Hit Sound.wav"));
 			this.hitSound = hitSound;
 		} catch (Exception ex) {
+			// print this if the sound doesnt run
 			System.out.println("Error with playing sound.");
 			ex.printStackTrace();
 		}
