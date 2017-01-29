80ce7cbea55e4a1bfdfc3b8506b552b3afd602ab
Pranav Gogia <gogiapranav@gmail.com>
2017-01-29 18:25:44 -0500

    Just added the group members names

--- Obstacles.java
+++ Obstacles.java
@@ -1,4 +1,8 @@
-
+// Team SNAP
+// Pranav G, Maryam S, Ajay G and Jennifer N.
+// Snake Game
+// This program is a reimagining of the classic game: "Snake"
+// 25 January 2017
 @SuppressWarnings("serial")
 public class Obstacles extends NonMoving {
 	public Obstacles() {
